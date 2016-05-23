04072b298633fb0bcf83a3e25a0d8251bc81e197
teleivo <mrteleivo@gmail.com>
2016-05-23 14:29:59 +0200

    fix serialization

--- openmrs-sdk.sh
+++ openmrs-sdk.sh
@@ -9,7 +9,9 @@ mvn openmrs-sdk:install -DserverId=platform2 -DgroupId=org.openmrs -DartifactId=
 mvn openmrs-sdk:install -DserverId=platform2 -DgroupId=org.openmrs.module -DartifactId=reporting -Dversion=0.9.8.1
 mvn openmrs-sdk:install -DserverId=platform2 -DgroupId=org.openmrs.module -DartifactId=htmlwidgets -Dversion=1.7.0
 mvn openmrs-sdk:install -DserverId=platform2 -DgroupId=org.openmrs.module -DartifactId=calculation -Dversion=1.1
-mvn openmrs-sdk:install -DserverId=platform2 -DgroupId=org.openmrs.module -DartifactId=serialization.xstream -Dversion=0.2.9
 mvn openmrs-sdk:install -DserverId=platform2 -DgroupId=org.openmrs.module -DartifactId=metadatasharing -Dversion=1.1.9
 mvn openmrs-sdk:install -DserverId=platform2 -DgroupId=org.openmrs.module -DartifactId=metadatamapping -Dversion=1.0.1
+# cant directly install serialization.xstream using SDK since its uploaded with extension .omod instead of .jar
+# therefore SDK cannot find it
+wget "http://mavenrepo.openmrs.org/nexus/content/repositories/modules/org/openmrs/module/serialization.xstream-omod/0.2.9/serialization.xstream-omod-0.2.9.omod" && mvn install:install-file -DgroupId=org.openmrs.module -DartifactId=serialization.xstream-omod -Dversion=0.2.9 -Dpackaging=jar -Dfile=serialization.xstream-omod-0.2.9.omod && mvn openmrs-sdk:install -DserverId=platform2 -DgroupId=org.openmrs.module -DartifactId=serialization.xstream -Dversion=0.2.9
 
