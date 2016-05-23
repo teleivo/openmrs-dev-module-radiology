9ea5f19da6e498e4d7ea6fb7c2cf3ce6cfb5bc2f
teleivo <mrteleivo@gmail.com>
2016-05-23 17:43:11 +0200

    add installation.properties for 116

--- setup-116.sh
+++ setup-116.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-mvn openmrs-sdk:setup-platform -DserverId=116 -Dversion=1.11.6
+mvn openmrs-sdk:setup-platform \
+    -DserverId="116" \
+    -Dversion="1.11.6" \
+    -DdbUser="openmrs" \
+    -DdbPassword="openmrs" \
+    -DdbDriver="mysql" \
+    -DdbUri="jdbc:mysql://localhost:3306/openmrs116?autoReconnect=true&sessionVariables=storage_engine=InnoDB&useUnicode=true&characterEncoding=UTF-8"
 
